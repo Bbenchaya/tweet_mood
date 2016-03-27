725dfab870ed7545843a7e698b8878b37ba53b93
asafch <asaf.chelouche@gmail.com>
2016-03-27 18:22:01 +0300

    boilerplate code for Local and Manager

---
+++ src/Local.java
@@ -0,0 +1,83 @@
+/**
+ * Created by asafchelouche on 25/3/16.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.UUID;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+public class Local {
+
+    private static final String bucket = "asafbendsp";
+    private static final String inputFileName = "/Users/asafchelouche/programming/tweet_mood/src/tweetLinks.txt";
+    private static final String objectName = "tweetLinks.txt";
+
+    public static void main(String[] args) throws IOException {
+        AmazonS3 s3 = new AmazonS3Client();
+        Region usEase1 = Region.getRegion(Regions.US_EAST_1);
+        s3.setRegion(usEase1);
+        System.out.println("Local client running...");
+        try {
+            System.out.println("Uploading a new object to S3 from a file\n");
+            File file = new File(inputFileName);
+            s3.putObject(new PutObjectRequest(bucket, objectName, file));
+//            System.out.println("Downloading an object");
+//            S3Object object = s3.getObject(new GetObjectRequest(bucket, objectName));
+//            System.out.println("Content-Type: "  + object.getObjectMetadata().getContentType());
+//            displayTextInputStream(object.getObjectContent());
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to Amazon S3, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with S3, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+    }
+
+    /**
+     * Displays the contents of the specified input stream as text.
+     *
+     * @param input
+     *            The input stream to display as text.
+     *
+     * @throws IOException
+     */
+    private static void displayTextInputStream(InputStream input) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) break;
+
+            System.out.println("    " + line);
+        }
+        System.out.println();
+    }
+
+}
---
+++ src/Worker.java
@@ -0,0 +1,5 @@
+/**
+ * Created by asafchelouche on 27/3/16.
+ */
+public class Worker {
+}
